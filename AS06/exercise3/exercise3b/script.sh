10329859e03df70a5fcf5ae892106542cd4c1c21
jakobudovic <ju8168@student.uni-lj.si>
2020-06-03 14:01:40 +0200

    finished exercise 3b

---
+++ AS06/exercise3/exercise3b/script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ip link add dev wg0 type wireguard
+ip address add dev wg0 10.90.42.2/24 dev wg0
+ip address add dev wg0 10.90.42.2 peer 10.90.42.0/24
+wg setconf wg0 /etc/wireguard/wg0.conf
+# wg set wg0 private-key 
+
+ip link set up dev wg0
--- AS06/exercise3/script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ip link add dev wg0 type wireguard
-# ip address add dev wg0 10.90.42.2/24
-ip address add dev wg0 10.90.42.2 peer 10.90.42.0/24
-wg setconf wg0 myconfig1.conf
-
-ip link set up dev wg0
